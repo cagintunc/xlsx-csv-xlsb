c50437033c032651c5a93f706e6e8f02515ce8cf
Çağın Tunç <71410956+cagintunc@users.noreply.github.com>
2022-08-21 17:31:40 +0300

    Initial commit

---
+++ PowerShell/program.ps1
@@ -0,0 +1,23 @@
+﻿Add-Type -AssemblyName Microsoft.Office.Interop.Excel
+$excel = New-Object -ComObject excel.application
+$excel.visible = $true
+
+$extens = "csv"
+$fileLocation = Read-Host -Prompt 'Program current path '
+$folderLoc = $fileLocation + "\rep"
+$files = (Get-ChildItem $folderLoc)
+
+
+ForEach($file in $files) {
+    cd $fileLocation
+    $path = ($file.fullname).substring(0, ($file.FullName).lastindexOf("."))
+    $workbook = $excel.workbooks.open($path) 
+    $path2 = $fileLocation + "\result\" + $file.Name.substring(0, ($file.Name).lastindexOf(".")) +"."+ $extens
+    $workbook.saveas($path2, 6)
+    $workbook.close()  	
+}
+
+$excel.Quit()
+$excel = $null
+[gc]::collect()
+[gc]::WaitForPendingFinalizers()
